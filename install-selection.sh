fdf89c34ed5856b1ebe373ebceeb94ac882fbb1b
neilsamaa <mirzamaulanaazmi@gmail.com>
2023-02-01 13:03:32 +0700

    fix identasi

--- install-selection.sh
+++ install-selection.sh
@@ -45,144 +45,144 @@ clear
 echo "What should i do?"
 echo
 echo "Select an options:"
-echo "   1) Update Ubuntu"
-echo "   2) Upgrade Ubuntu"
-echo "   3) Install Docker"
-echo "   4) Install Portainer"
-echo "   5) Exit"
+echo "  1) Update Ubuntu"
+echo "  2) Upgrade Ubuntu"
+echo "  3) Install Docker"
+echo "  4) Install Portainer"
+echo "  5) Exit"
 read -p "Option: " input
 until [[ "$input" =~ ^[1-5]$ ]]; do
-		echo "$input: invalid selection."
-		read -p "Option: " input
+	echo "$input: invalid selection."
+	read -p "Option: " input
 done
 case "$input" in
     1)
-      read -p "Are you sure to update ubuntu? [y/n]: " input
-      until [[ "$input" =~ ^[yYnN]*$ ]]; do
-          echo "$input: invalid selection."
-          read -p "Are you sure to update ubuntu? [y/n]: " input
-      done
-      if [[ "$input" =~ ^[yY]$ ]]; then
-          update
-          echo
-          echo "Ubuntu has been updated"
-      else
-          echo
-          echo "Update aborted!"
-      fi
-      # BACK TO MENU
-      echo
-      read -p "Are you want back to menu? [y/n]: " input
-      until [[ "$input" =~ ^[yYnN]*$ ]]; do
-          echo "$input: invalid selection."
-          read -p "Are you want back to menu? [y/n]: " input
-      done
-      if [[ "$input" =~ ^[yY]$ ]]; then
-          menu
-      else
-          exit
-      fi
+        read -p "Are you sure to update ubuntu? [y/n]: " input
+        until [[ "$input" =~ ^[yYnN]*$ ]]; do
+            echo "$input: invalid selection."
+            read -p "Are you sure to update ubuntu? [y/n]: " input
+        done
+        if [[ "$input" =~ ^[yY]$ ]]; then
+            update
+            echo
+            echo "Ubuntu has been updated"
+        else
+            echo
+            echo "Update aborted!"
+        fi
+        # BACK TO MENU
+        echo
+        read -p "Are you want back to menu? [y/n]: " input
+        until [[ "$input" =~ ^[yYnN]*$ ]]; do
+            echo "$input: invalid selection."
+            read -p "Are you want back to menu? [y/n]: " input
+        done
+        if [[ "$input" =~ ^[yY]$ ]]; then
+            menu
+        else
+            exit
+        fi
     ;;
     2)
-      read -p "Are you sure to upgrade ubuntu? [y/n]: " input
-      until [[ "$input" =~ ^[yYnN]*$ ]]; do
-          echo "$input: invalid selection."
-          read -p "Are you sure to upgrade ubuntu? [y/n]: " input
-      done
-      if [[ "$input" =~ ^[yY]$ ]]; then
-          upgrade
-          echo
-          echo "Ubuntu has been upgraded"
-      else
-          echo
-          echo "Upgrade aborted!"
-      fi
-      # BACK TO MENU
-      echo
-      read -p "Are you want back to menu? [y/n]: " input
-      until [[ "$input" =~ ^[yYnN]*$ ]]; do
-          echo "$input: invalid selection."
-          read -p "Are you want back to menu? [y/n]: " input
-      done
-      if [[ "$input" =~ ^[yY]$ ]]; then
-          menu
-      else
-          exit
-      fi
+        read -p "Are you sure to upgrade ubuntu? [y/n]: " input
+        until [[ "$input" =~ ^[yYnN]*$ ]]; do
+            echo "$input: invalid selection."
+            read -p "Are you sure to upgrade ubuntu? [y/n]: " input
+        done
+        if [[ "$input" =~ ^[yY]$ ]]; then
+            upgrade
+            echo
+            echo "Ubuntu has been upgraded"
+        else
+            echo
+            echo "Upgrade aborted!"
+        fi
+        # BACK TO MENU
+        echo
+        read -p "Are you want back to menu? [y/n]: " input
+        until [[ "$input" =~ ^[yYnN]*$ ]]; do
+            echo "$input: invalid selection."
+            read -p "Are you want back to menu? [y/n]: " input
+        done
+        if [[ "$input" =~ ^[yY]$ ]]; then
+            menu
+        else
+            exit
+        fi
     ;;
     3)
-      read -p "Are you sure to Install Docker? [y/n]: " input
-      until [[ "$input" =~ ^[yYnN]*$ ]]; do
-          echo "$input: invalid selection."
-          read -p "Are you sure to Install Docker? [y/n]: " input
-      done
-      if [[ "$input" =~ ^[yY]$ ]]; then
-          update
-          read -p "Are you sure to upgrade ubuntu? (recommend upgrade) [y/n]: " input
-          until [[ "$input" =~ ^[yYnN]*$ ]]; do
-              echo "$input: invalid selection."
-              read -p "Are you sure to upgrade ubuntu? (recommend upgrade) [y/n]: " input
-          done
-          if [[ "$input" =~ ^[yY]$ ]]; then
-              upgrade
-              echo 
-              echo "Ubuntu has been upgraded"
-              echo 
-              docker
-              echo
-              echo "Docker has been installed"
-          else
-              docker
-              echo
-              echo "Docker has been installed"
-          fi
-          menu
-      else
-          echo
-          echo "Install Docker aborted!"
-      fi
-      # BACK TO MENU
-      echo
-      read -p "Are you want back to menu? [y/n]: " input
-      until [[ "$input" =~ ^[yYnN]*$ ]]; do
-          echo "$input: invalid selection."
-          read -p "Are you want back to menu? [y/n]: " input
-      done
-      if [[ "$input" =~ ^[yY]$ ]]; then
-          menu
-      else
-          exit
-      fi
+        read -p "Are you sure to Install Docker? [y/n]: " input
+        until [[ "$input" =~ ^[yYnN]*$ ]]; do
+            echo "$input: invalid selection."
+            read -p "Are you sure to Install Docker? [y/n]: " input
+        done
+        if [[ "$input" =~ ^[yY]$ ]]; then
+            update
+            read -p "Are you sure to upgrade ubuntu? (recommend upgrade) [y/n]: " input
+            until [[ "$input" =~ ^[yYnN]*$ ]]; do
+                echo "$input: invalid selection."
+                read -p "Are you sure to upgrade ubuntu? (recommend upgrade) [y/n]: " input
+            done
+            if [[ "$input" =~ ^[yY]$ ]]; then
+                upgrade
+                echo 
+                echo "Ubuntu has been upgraded"
+                echo 
+                docker
+                echo
+                echo "Docker has been installed"
+            else
+                docker
+                echo
+                echo "Docker has been installed"
+            fi
+            menu
+        else
+            echo
+            echo "Install Docker aborted!"
+        fi
+        # BACK TO MENU
+        echo
+        read -p "Are you want back to menu? [y/n]: " input
+        until [[ "$input" =~ ^[yYnN]*$ ]]; do
+            echo "$input: invalid selection."
+            read -p "Are you want back to menu? [y/n]: " input
+        done
+        if [[ "$input" =~ ^[yY]$ ]]; then
+            menu
+        else
+            exit
+        fi
     ;;
     4)
-      read -p "Are you sure to install portainer? [y/n]: " input
-      until [[ "$input" =~ ^[yYnN]*$ ]]; do
-          echo "$input: invalid selection."
-          read -p "Are you sure to install portainer? [y/n]: " input
-      done
-      if [[ "$input" =~ ^[yY]$ ]]; then
-          input
-          echo
-          echo "Portainer has been installed"
-      else
-          echo
-          echo "Install Portainer aborted!"
-      fi
-      # BACK TO MENU
-      echo
-      read -p "Are you want back to menu? [y/n]: " input
-      until [[ "$input" =~ ^[yYnN]*$ ]]; do
-          echo "$input: invalid selection."
-          read -p "Are you want back to menu? [y/n]: " input
-      done
-      if [[ "$input" =~ ^[yY]$ ]]; then
-          menu
-      else
-          exit
-      fi
+        read -p "Are you sure to install portainer? [y/n]: " input
+        until [[ "$input" =~ ^[yYnN]*$ ]]; do
+            echo "$input: invalid selection."
+            read -p "Are you sure to install portainer? [y/n]: " input
+        done
+        if [[ "$input" =~ ^[yY]$ ]]; then
+            input
+            echo
+            echo "Portainer has been installed"
+        else
+            echo
+            echo "Install Portainer aborted!"
+        fi
+        # BACK TO MENU
+        echo
+        read -p "Are you want back to menu? [y/n]: " input
+        until [[ "$input" =~ ^[yYnN]*$ ]]; do
+            echo "$input: invalid selection."
+            read -p "Are you want back to menu? [y/n]: " input
+        done
+        if [[ "$input" =~ ^[yY]$ ]]; then
+            menu
+        else
+            exit
+        fi
     ;;
     5)
-      exit
+        exit
     ;;
 	esac
 }
