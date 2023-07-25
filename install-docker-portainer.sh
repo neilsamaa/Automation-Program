7fcce934a73723c5aec5d37db50201a3d92ca86d
Mirza Maulana Azmi <55909176+neilsamaa@users.noreply.github.com>
2023-07-25 12:46:43 +0700

    automate install docker portainer using bash

--- install-docker-portainer.sh
+++ install-docker-portainer.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
-sudo apt-get update -y;
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y;
-sudo mkdir -m 0755 -p /etc/apt/keyrings;
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg;
+# Update package lists
+echo "Updating package lists..."
+sudo apt-get update -y
+
+echo "Installing prerequisite packages..."
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+
+# Add Docker's official GPG key
+echo "Adding Docker's official GPG key..."
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Set up the stable repository
+echo "Setting up the stable repository..."
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
-sudo apt-get update -y;
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y;
-sudo service docker start;
-sudo docker pull portainer/portainer-ce;
-sudo docker volume create portainer_data;
-sudo docker run -d \
-    -p 8000:8000 \
-    -p 9443:9443 \
-    --name portainer \
-    --restart=always \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v portainer_data:/data \
-portainer/portainer-ce:latest;
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package lists
+echo "Updating package lists..."
+sudo apt-get update -y
+
+# Install Docker Services
+echo "Installing Docker CE..."
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Running Portainer as a Docker container
+echo "Running Portainer..."
+sudo docker pull portainer/portainer-ce
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
